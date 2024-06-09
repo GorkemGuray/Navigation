49d62d37ef9f321224082de317323fd2e1a39cb8
Görkem Güray <grkmgry@gmail.com>
2024-06-09 14:41:26 +0300

    day 44

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,31 +7,68 @@
 
 import SwiftUI
 
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        // Still here? Start with an empty path.
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+    
+    
+}
+
+
+
 struct DetailView: View {
     var number: Int
     
     var body: some View {
-        Text("Detail View \(number)")
-    }
-    
-    init(number: Int) {
-        self.number = number
-        print("Creating detail view\(number)")
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
     }
 }
 
 struct ContentView: View {
+    
+    @State private var pathStore = PathStore()
+    
     var body: some View {
-        NavigationStack {
-            List(0..<100) {i in
-                NavigationLink("Select \(i)", value: i)
-            }
-            .navigationDestination(for: Int.self) {selection in
-                Text("You selected \(selection)")
-            }
-        }
-    }
-}
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0)
+                .navigationDestination(for: Int.self) {i in
+                    DetailView(number: i)
+                }
+           
+        } //NavigationStack
+
+    } //var body
+} //ContentView
 
 #Preview {
     ContentView()
